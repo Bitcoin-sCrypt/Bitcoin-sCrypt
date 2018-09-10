e1ea8b9cf7efc3b8312522ccc630be374ca6ed58
Spidersbox <Spider@Spidersbox.com>
2018-09-09 22:45:20 -0700

    more proofofwork debug

--- src/main.cpp
+++ src/main.cpp
@@ -1374,12 +1374,14 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits)
     bnTarget.SetCompact(nBits);
 
     // Check range
+printf("target %s \n",bnTarget.GetHex().c_str());
+printf("hash   %s \n",hash.GetHex().c_str());
     if (bnTarget <= 0 || bnTarget > bnProofOfWorkLimit)
         return error("CheckProofOfWork() : nBits below minimum work");
 
     // Check proof of work matches claimed amount
     if (hash > bnTarget.getuint256())
-        return error("CheckProofOfWork() : hash doesn't match nBits");
+        return error("CheckProofOfWork() : hash doesn't match nBits (hash > bntraget)\n");
 
     return true;
 }
