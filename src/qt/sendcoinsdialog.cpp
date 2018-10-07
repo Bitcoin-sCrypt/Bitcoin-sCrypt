18e516e9372318600da06393cd6c0d6d47c0d5eb
spider <spider@spiders-iMac.local>
2018-10-07 01:32:39 -0700

    text correction

--- src/qt/sendcoinsdialog.cpp
+++ src/qt/sendcoinsdialog.cpp
@@ -36,7 +36,7 @@ SendCoinsDialog::SendCoinsDialog(QWidget *parent) :
 
 #if QT_VERSION >= 0x040700
      /* Do not move this to the XML file, Qt before 4.7 will choke on it */
-     ui->lineEditCoinControlChange->setPlaceholderText(tr("Enter a LitecoinPlus address"));
+     ui->lineEditCoinControlChange->setPlaceholderText(tr("Enter a Bitcoin-sCrypt address"));
 #endif
 
     addEntry();
--- src/qt/sendcoinsentry.cpp
+++ src/qt/sendcoinsentry.cpp
@@ -23,7 +23,7 @@ SendCoinsEntry::SendCoinsEntry(QWidget *parent) :
 #if QT_VERSION >= 0x040700
     /* Do not move this to the XML file, Qt before 4.7 will choke on it */
     ui->addAsLabel->setPlaceholderText(tr("Enter a label for this address to add it to your address book"));
-    ui->payTo->setPlaceholderText(tr("Enter a valid LitecoinPlus address"));
+    ui->payTo->setPlaceholderText(tr("Enter a valid Bitcoin-sCrypt address"));
 #endif
     setFocusPolicy(Qt::TabFocus);
     setFocusProxy(ui->payTo);
